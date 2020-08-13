f0bf6c46ee2322fdeb0f4c6574ed0d93a25c8ffb
ryanzomorrodi <ryanzomorrodi@gmail.com>
2020-08-13 14:18:51 -0500

    Add files via upload

--- mainpageScroll.js
+++ mainpageScroll.js
@@ -1,23 +1,21 @@
 $(document).ready(function() {
   $(window).scroll(function() {
     if ($(window).scrollTop() > ($(window).height() / 2) - 10) {
+      $("#aboutHeader").addClass('aboutHeaderFixed');
+      $("#aboutHeader").css("left","-470px");
       $('#headerName').addClass('headerNameFixed');
-      $('#aboutHeader').animate({
-        display: 'none'
-      });
     }
     if ($(window).scrollTop() < ($(window).height() / 2) - 10) {
+      $("#aboutHeader").removeClass('aboutHeaderFixed');
+      $("#aboutHeader").css("left","0px")
       $('#headerName').removeClass('headerNameFixed');
-      $('#aboutHeader').animate({
-        display: 'inline'
-      });
     }
     var pass = $(window).height() - 60;
     if ($(window).scrollTop() > pass) {
       $("#header").css({
-        "backgroundColor": "#111"
+        "backgroundColor": "#222",
+        "box-shadow": "0 2px 0 rgba(0, 0, 0, 0.4)"
       });
-      $("#header").css('box-shadow', '0 2px 0 rgba(0, 0, 0, 0.4)');
       $("#headerName").css({
         "color": "white"
       });
@@ -28,9 +26,9 @@ $(document).ready(function() {
     }
     if ($(window).scrollTop() < pass) {
       $("#header").css({
-        "background": "none"
+        "background": "none",
+        "box-shadow": "none"
       });
-      $("#header").css('box-shadow', 'none');
       $("#headerName").css({
         "color": "white"
       });
